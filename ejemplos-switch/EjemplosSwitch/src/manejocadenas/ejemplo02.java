9f0dd28a60965c5c8a887e58273b3e7f8c6f0d07
Erick Gaona <erickgaona@gmail.com>
2022-11-16 15:50:56 -0500

    actividad en clase

---
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/ejemplo02.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejocadenas;
+
+/**
+ *
+ * @author SALA I
+ */
+public class ejemplo02 {
+    
+    public static void main(String[] args) {
+        String pais;
+        pais= "Usa";
+        char letra = pais.charAt(0);
+        System.out.printf("%s\n", pais.charAt(0));
+        
+    }
+   
+    
+    
+}
